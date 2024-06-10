5de7d6ec38089400f0f7daad11a9fb7fe2c04076
bvvst <lukeimbing@gmail.com>
2024-06-10 17:24:55 -0400

    og image and head refactor

---
+++ src/routes/(normal)/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../../app.css";
+</script>
+
+<svelte:head>
+  <title>Guess the Puffle's Color!</title>
+
+  <meta property="og:title" content="Guess the puffle's color" />
+  <meta property="og:image" content="https://i.imgur.com/u24nu9r.png" />
+  <meta property="og:description" content="Guess the puffle's color and win!" />
+</svelte:head>
+
+<slot />
---
+++ src/routes/share/[id]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { env } from "$env/dynamic/public";
+
+  export let data;
+  console.log(data.id);
+</script>
+
+<svelte:head>
+  <title>bruh moment</title>
+  <meta property="og:title" content="Guess the puffle's color" />
+  <meta property="og:image" content="{env.PUBLIC_BACKEND_URL}/{data.id}" />
+  <meta property="og:description" content="Guess the puffle's color and win!" />
+</svelte:head>
+
+<h1>hi</h1>
