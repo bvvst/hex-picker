f7754318b8ce29d68c8988bff1f27f25fd773fb0
bvvst <lukeimbing@gmail.com>
2024-06-10 18:15:24 -0400

    more tags

--- src/routes/share/[id]/+page.svelte
+++ src/routes/share/[id]/+page.svelte
@@ -10,6 +10,10 @@
   <meta property="og:title" content="Check out my hex color score!" />
   <meta property="og:image" content="{env.PUBLIC_BACKEND_URL}/{data.id}" />
   <meta property="og:description" content="Guess hex colors!" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="Check out my hex color score!" />
+  <meta name="twitter:description" content="Guess hex colors!" />
+  <meta name="twitter:image" content="{env.PUBLIC_BACKEND_URL}/{data.id}" />
 </svelte:head>
 
 <h1>hi</h1>
