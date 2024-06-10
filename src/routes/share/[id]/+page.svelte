640acaeaca11ae7bc4188da1a8fea8719cfb53bf
bvvst <lukeimbing@gmail.com>
2024-06-10 18:09:10 -0400

    refactor: Update og:title and og:description in +page.svelte

--- src/routes/share/[id]/+page.svelte
+++ src/routes/share/[id]/+page.svelte
@@ -7,9 +7,9 @@
 
 <svelte:head>
   <title>bruh moment</title>
-  <meta property="og:title" content="Guess the puffle's color" />
+  <meta property="og:title" content="Check out my hex color score!" />
   <meta property="og:image" content="{env.PUBLIC_BACKEND_URL}/{data.id}" />
-  <meta property="og:description" content="Guess the puffle's color and win!" />
+  <meta property="og:description" content="Guess hex colors!" />
 </svelte:head>
 
 <h1>hi</h1>
